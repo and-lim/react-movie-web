5ac14f7eb0fbcbed4207d4aa4d2326a6efa916eb
and-lim <andrianlimnardy@gmail.com>
2023-09-28 17:29:53 +0700

    first commit

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -96,7 +96,7 @@ const Movie=()=>{
                                 </div>
                                 <input type="search" id="default-search"  onChange={(e)=>{setSearch(e.target.value)}} 
                                     value={search} onKeyPress={searchMovie} class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search Movie" />
-                                <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
+                                <button type="submit" class="text-black absolute right-2.5 bottom-2.5  bg-gradient-to-r from-lime-300 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
                             </div>
                         </form>
 
